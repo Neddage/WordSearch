3601ed032a05e5a3592216e11e1bb9c2062d8a9c
Ian Shaw <ian.shaw@digitaltactics.co.uk>
2023-06-06 12:23:40 +0100

    interface for a "Placer"

---
+++ word_search/word_placer_interface.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractclassmethod
+from word_search.word_search import WordSearch
+
+class WordPlacerInterface(ABC):
+    @abstractclassmethod
+    def place_word(self, word:str, word_search: WordSearch) -> WordSearch:
+        pass
