2151f7ddc460cf858ea19d01759c63ab273cbaf7
Ian Shaw <ian.shaw@digitaltactics.co.uk>
2023-06-06 15:08:36 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='word_search',
+    version='0.0.1',    
+    description='A Python package for Word Searches',
+    url='https://github.com/Neddage/WordSearch',
+    author='Ian Shaw',
+    author_email='ian@ianshaw.me',
+    license='MIT',
+    packages=['word_search'],
+    install_requires=[]
+)
