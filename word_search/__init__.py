3ab21f37019accccdfea404ce6e8c43fc6209ce7
Ian Shaw <ian.shaw@digitaltactics.co.uk>
2023-06-06 17:00:31 +0100

    package __init__.py

--- word_search/__init__.py
+++ word_search/__init__.py
@@ -3,6 +3,13 @@ word_search
 
 A Python module to generate and solve word searches.
 """
+__all__ = ["WordSearch", "WordPlacerInterface", "WordPlacement", "HorizontalWordPlacer"]
 __version__ = "0.0.1"
 __author__ = 'Ian Shaw'
 __credits__ = ''
+
+# Importing the necessary modules and classes
+from word_search.word_search import WordSearch
+from word_search.word_placer_interface import WordPlacerInterface
+from word_search.word_placement import WordPlacement
+from word_search.horizontal_word_placer import HorizontalWordPlacer
