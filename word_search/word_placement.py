fd6aa5eb930d67fe0ea5e8eb845826c59e19561d
Ian Shaw <ian.shaw@digitaltactics.co.uk>
2023-06-06 16:42:50 +0100

    added property setter and validation to WordPlacement

--- word_search/word_placement.py
+++ word_search/word_placement.py
@@ -9,3 +9,50 @@ class WordPlacement:
         """
         self.word = word
         self.positions = positions
+        
+    @property
+    def positions(self):
+        """ Returns a dictionary with the key as a string representation of the row and column and the value the letter
+
+        Returns:
+            dict[str, str]: dictionary of position to letter ie. { "0-1": "t", "0-2": "e", "0-3": "s", "0-4": "t"}
+        """
+        return self._positions
+    
+    @positions.setter
+    def positions(self, positions: dict[str, str]) -> None:
+        """ Setter for positions. Validates the positions before storing.
+
+        Args:
+            positions (dict[str, str]): dictionary of position to letter. ie. { "0-1": "t", "0-2": "e", "0-3": "s", "0-4": "t"}
+        """
+        self.validate_positions(positions)
+        self._positions = positions
+        
+    @staticmethod
+    def validate_positions(positions: dict[str, str]) -> None:
+        """ Validates the supplied positions dict 
+
+        Args:
+            positions (dict[str, str]): dictionary of position to letter. ie. { "0-1": "t", "0-2": "e", "0-3": "s", "0-4": "t"}
+
+        Raises:
+            ValueError: Both key and value in positions should be strings
+            ValueError: The key should be a string of two numbers separated by -
+            ValueError: The value should be a single letter
+        """
+        # TODO: Should probably expand validation in the future to allow more flexibility
+        if type(positions) is not dict:
+            raise TypeError("Positions must be a dict")
+        
+        for key, value in positions.items():
+            if not isinstance(key, str) or not isinstance(value, str):
+                raise TypeError("Both key and value in positions should be strings")
+
+            row, column = key.split("-")
+            if not row.isdigit() or not column.isdigit():
+                raise ValueError("The key should be a string of two numbers separated by -")
+
+            if len(value) != 1 or not value.isalpha():
+                raise ValueError("The value should be a single letter")
+    
