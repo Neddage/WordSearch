09f1231fa6c1cc620dbc4f28ee4e2430ef2947c5
Ian Shaw <ian.shaw@digitaltactics.co.uk>
2023-06-06 13:27:00 +0100

    comments

--- word_search/word_search.py
+++ word_search/word_search.py
@@ -4,48 +4,76 @@ from word_search.word_placement import WordPlacement
 
 class WordSearch:
     def __init__(self, width: int, height: int) -> None:
+        """ Class constructor
+
+        Args:
+            width (int): The desired width of the word search (ie. columns)
+            height (int): The desired height of the word search (ie. rows)
+        """
         self._width = width
         self._height = height
         # initialise the grid
         self._initialise_grid()
         # initialise the dict to hold the word objects
         self._word_placements = {}
-        
-    @property
-    def width(self):
-        return self._width
     
-    @property
-    def height(self):
-        return self._height
-    
-    def can_place(self, placement: WordPlacement):
+    def can_place(self, placement: WordPlacement) -> bool:
+        """ Checks to see if the supplied WordPlacement can be added to the word search without overlapping an existing word
+            
+        Args:
+            placement (WordPlacement): The WordPlacement object to check
+
+        Returns:
+            bool: True if it can be placed on the WordSearch False otherwise
+        """
         all = self.get_positions_in_use()
         if not all:
+            # if no words placed, then nothing to check
             return True
         
+        # TODO: Should allow cases where the letter being overwritten is the same as the existing one so words can overlap when suitable
         for position in placement.positions:
             if position in all:
+                # return False as soon as we find a clash, no point continuing to check
                 return False
             
         return True
     
     def get_positions_in_use(self) -> list[str]:
+        """ Returns an array of string representations of all the WordSearch grid positions currently being used
+
+        Returns:
+            list[str]: array of string representations of positions. ie. ["1-0", "1-1", "1-2"]
+        """
         in_use = []
+        # I feel there is a more pythonic way of doing this but for right now we loop through and build the positions array
         for placement in self._word_placements:
             in_use.extend(self._word_placements[placement].positions)
         return in_use
     
     def remove_word(self, word: str) -> None:
+        """ Removes a word from the WordSearch object and grid
+
+        Args:
+            word (str): The word to remove
+        """
         if word in self._word_placements:
             # get each of the positions and "reset" them to a random character
             for position in self._word_placements[word].positions:
-                print(position)
                 row, col = position.split("-")
                 self._grid[int(row)][int(col)] = random.choice('abcdefghijklmnopqrstuvwxyz')
+            # remove the word placement object out the list
             del self._word_placements[word]
         
     def add_word_placement(self, placement: WordPlacement) -> None:
+        """ Adds a WordPlacement into the WordSearch
+
+        Args:
+            placement (WordPlacement): The WordPlacement object to add
+
+        Raises:
+            UnableToPlaceWordError: Raised if we cannot place the word on the grid with the current positions
+        """
         # remove existing word if it already exists
         self.remove_word(placement.word)
         # verify that none of the positions in the placement have already been used
@@ -59,16 +87,40 @@ class WordSearch:
         self._word_placements[placement.word] = placement
     
     def _initialise_grid(self) -> None:
+        """ Initialise the WordSearch grid based on the width/height and fills it with random characters
+        """
         # Build the grid, inserting random characters into the slots
         self._grid = [["" for _ in range(self._width)] for _ in range(self._height)]
+        # TODO: Probably don't need to use a separate method here now
         self._fill_empty()
     
     def _fill_empty(self):
+        """ Replaces any blank entries in the grid with a random character
+        """
         # Fill any empty spaces with a random letter
         for row in range(self._height):
             for col in range(self._width):
                 if not self._grid[row][col]:
                     self._grid[row][col] = random.choice('abcdefghijklmnopqrstuvwxyz')
+                    
+    @property
+    def width(self) -> int:
+        """ Width property
+
+        Returns:
+            int: The width
+        """
+        return self._width
     
+    @property
+    def height(self) -> int:
+        """ Height property
+
+        Returns:
+            int: The Height
+        """
+        return self._height
+    
+# Custom error to indicate word placement failed
 class UnableToPlaceWordError(RuntimeError):
     pass
